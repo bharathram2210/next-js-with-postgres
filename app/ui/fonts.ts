1e31e313c30c177fc6faf2e032dd2d521243ef66
bharathrams <bharathram@navadhiti.com>
2025-03-18 15:00:08 +0530

    added the project in the github

---
+++ app/ui/fonts.ts
@@ -0,0 +1,8 @@
+import { Inter, Lusitana,Josefin_Sans } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['vietnamese'] });
+
+export const lusitana = Josefin_Sans({
+    weight: ['400', '700'],
+    subsets: ['latin'],
+  });
